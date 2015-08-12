212e680da5fc6494d9859d0a23340463fe4967e5
darthbatman <darthanakin44@gmail.com>
2015-08-11 21:23:34 -0400

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace WordDocTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.docOutput = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(46, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(326, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open Word Document";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // docOutput
+            // 
+            this.docOutput.FormattingEnabled = true;
+            this.docOutput.ItemHeight = 31;
+            this.docOutput.Location = new System.Drawing.Point(46, 160);
+            this.docOutput.Name = "docOutput";
+            this.docOutput.Size = new System.Drawing.Size(1007, 686);
+            this.docOutput.TabIndex = 1;
+            this.docOutput.SelectedIndexChanged += new System.EventHandler(this.docOutput_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(334, 909);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(79, 38);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 912);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(274, 32);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Vocabulary Chapter:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(465, 902);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(208, 57);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Save Test";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 31;
+            this.listBox1.Location = new System.Drawing.Point(1203, 160);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(945, 686);
+            this.listBox1.TabIndex = 5;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(2221, 1049);
+            this.tabControl1.TabIndex = 6;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label9);
+            this.tabPage1.Controls.Add(this.label8);
+            this.tabPage1.Controls.Add(this.label6);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.listBox1);
+            this.tabPage1.Controls.Add(this.docOutput);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(10, 48);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(2201, 991);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Save Tests";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label7);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.textBox5);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.textBox4);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.textBox3);
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Location = new System.Drawing.Point(10, 48);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(2201, 991);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Create Cumulative";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 32);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Chapters:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(178, 60);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(84, 38);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(307, 60);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(86, 38);
+            this.textBox3.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(277, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 32);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "-";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(246, 313);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(231, 49);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Generate Test";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(737, 60);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(87, 38);
+            this.textBox4.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(441, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(290, 32);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Number of Questions:";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(354, 179);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(470, 38);
+            this.textBox5.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(40, 182);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(288, 32);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Word Document Title:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(1666, 956);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(535, 32);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Developed by Rishi Masand August 2015";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(1666, 956);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(535, 32);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Developed by Rishi Masand August 2015";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(43, 109);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(370, 32);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Contents of Word Document";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(1197, 109);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(365, 32);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Contents of Destination File";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2247, 1073);
+            this.Controls.Add(this.tabControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Opacity = 0.92D;
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MEK Vocabulary Test Generator";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox docOutput;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Word;
+
+namespace WordDocTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MessageBox.Show("MEK Vocabulary Test Generator\nDeveloped by Rishi Masand August 2015 for MEK Review");
+        }
+        
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Word Documents (*.docx)|*.docx";
+            ofd.ShowDialog();
+
+            string filePath;
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                filePath = ofd.FileName;
+
+                Microsoft.Office.Interop.Word.Application word = new Microsoft.Office.Interop.Word.Application();
+                object miss = System.Reflection.Missing.Value;
+                object path = filePath;
+
+                Microsoft.Office.Interop.Word.Application application = new Microsoft.Office.Interop.Word.Application();
+                Document document = application.Documents.Open(path);
+
+                docOutput.Items.Clear();
+                listBox1.Items.Clear();
+
+                string totaltext = "";
+                for (int i = 0; i < document.Paragraphs.Count; i++)
+                {
+                    totaltext += " \r\n " + document.Paragraphs[i + 1].Range.Text.ToString();
+                    docOutput.Items.Add(document.Paragraphs[i + 1].Range.Text.ToString());
+
+
+                }
+
+                string theSource = totaltext;
+                string[] theStringSeparators = new string[] { "\n" };
+                string[] theResult;
+                theResult = theSource.Split(theStringSeparators, StringSplitOptions.None);
+
+                string writePath = System.IO.Directory.GetCurrentDirectory();
+                Console.Write(writePath);
+
+                System.Text.StringBuilder sb = new System.Text.StringBuilder();
+                sb.Append(writePath).Append("\\questionCollection");
+
+
+                for (int w = 0; w < theResult.Count(); w++)
+                {
+
+
+
+
+
+                    if (theResult[w][1] == 'A' || theResult[w][1] == 'B' || theResult[w][1] == 'C' || theResult[w][1] == 'D' || theResult[w][1] == 'E')
+                    {
+                        if (theResult[w][1] == 'A')
+                        {
+                            listBox1.Items.Add(theResult[w - 1]);
+
+                        }
+                        if (theResult[w][2] == '.')
+                        {
+                            listBox1.Items.Add(theResult[w]);
+
+                        }
+                        //if (theResult[w][1] == 'E')
+                        //{
+                        //    listBox1.Items.Add("----");
+
+                        //}
+                    }
+
+
+                }
+
+
+                ((Microsoft.Office.Interop.Word._Application)application).Quit();
+            }
+
+
+            
+
+
+        }
+
+        private void docOutput_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                int m = Int32.Parse(textBox1.Text);
+                string writePath = System.IO.Directory.GetCurrentDirectory();
+                Console.Write(writePath);
+
+                System.Text.StringBuilder sb = new System.Text.StringBuilder();
+                sb.Append(writePath).Append("\\questionCollection.txt");
+
+                System.IO.StreamWriter SaveFile = new System.IO.StreamWriter(sb.ToString(), true);
+                SaveFile.WriteLine("****");
+                SaveFile.WriteLine(m.ToString());
+                foreach (var item in listBox1.Items)
+                {
+                    SaveFile.WriteLine(item);
+                }
+
+                SaveFile.Close();
+
+                MessageBox.Show("Test saved!");
+            }
+            catch (FormatException we)
+            {
+                //Console.WriteLine(we.Message);
+                MessageBox.Show("Test Number must be an Integer");
+            }
+
+
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            Microsoft.Office.Interop.Word.Application application = new Microsoft.Office.Interop.Word.Application();
+            var document = application.Documents.Add();
+            //var paragraph = document.Paragraphs.Add();
+            //paragraph.Range.Text = "some text";
+
+            //string filename = "testdoc";
+            //application.ActiveDocument.SaveAs(filename, WdSaveFormat.wdFormatDocument);
+            //((Microsoft.Office.Interop.Word._Document)document).Close();
+            //MessageBox.Show("File saved in Documents");
+            //document.Close();
+
+            int fromChapter = Int32.Parse(textBox2.Text);
+            int toChapter = Int32.Parse(textBox3.Text);
+
+            int chapDifference = toChapter - fromChapter;
+
+            string writePath = System.IO.Directory.GetCurrentDirectory();
+            Console.Write(writePath);
+
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            sb.Append(writePath).Append("\\questionCollection.txt");
+
+            string[] lines = System.IO.File.ReadAllLines(sb.ToString());
+
+            Random rnd = new Random(DateTime.UtcNow.Millisecond);
+            int questionNum = rnd.Next(1, 31);
+
+            Random rndChap = new Random(DateTime.UtcNow.Millisecond);
+            int chapNum = rndChap.Next(fromChapter, (toChapter + 1));
+
+            bool correctChapterFound = false;
+
+            int[] usedQuestions = new int[1250];
+
+            bool notUsedBefore = true;
+
+            int numOfQuestions = Int32.Parse(textBox4.Text);
+
+            StringBuilder wordDocBuilder = new StringBuilder();
+            wordDocBuilder.Append(textBox5.Text).Append("\nName: ___________________________\n").AppendLine("Class: ___________________________");
+
+            for (int u = 0; u < numOfQuestions; u++)
+            {
+                chapNum = rndChap.Next(fromChapter, (toChapter + 1));
+                for (int v = 0; v < lines.Count(); v++)
+                {
+                    if (lines[v] == "****")
+                    {
+                        if (lines[v + 1] == chapNum.ToString())
+                        {
+                            //MessageBox.Show(lines[v + 1]);
+                            
+                            questionNum = rnd.Next(1, 30);
+                            int numToUse = questionNum * 18;
+                            //MessageBox.Show(numToUse.ToString());
+                            //MessageBox.Show(lines[v + numToUse + 2]);
+                            
+
+                            for (int o = 0; o < usedQuestions.Count(); o++)
+                            {
+                                if (usedQuestions[o] == v + numToUse + 2)
+                                {
+                                    notUsedBefore = false;
+                                    questionNum = rnd.Next(1, 30);
+                                    numToUse = questionNum * 18;
+                                    o = 0;
+                                }
+                                else
+                                {
+                                    notUsedBefore = true;
+                                }
+                            }
+
+
+                            var output = System.Text.RegularExpressions.Regex.Replace(lines[v + numToUse + 2], @"[\d-]", string.Empty);
+                            
+
+                                if (notUsedBefore)
+                                {
+                                    usedQuestions[u] = v + numToUse + 2;
+                                    //MessageBox.Show(output);
+                                    //MessageBox.Show(lines[v + numToUse + 5]);
+                                    //MessageBox.Show(lines[v + numToUse + 8]);
+                                    //MessageBox.Show(lines[v + numToUse + 11]);
+                                    //MessageBox.Show(lines[v + numToUse + 14]);
+                                    //MessageBox.Show(lines[v + numToUse + 17]);
+
+                                    StringBuilder outputBuild = new StringBuilder();
+                                    outputBuild.Append((u + 1).ToString()).Append(output);
+
+                                    wordDocBuilder.AppendLine(outputBuild.ToString());
+                                    wordDocBuilder.AppendLine(lines[v + numToUse + 5]);
+                                    wordDocBuilder.AppendLine(lines[v + numToUse + 8]);
+                                    wordDocBuilder.AppendLine(lines[v + numToUse + 11]);
+                                    wordDocBuilder.AppendLine(lines[v + numToUse + 14]);
+                                    wordDocBuilder.AppendLine(lines[v + numToUse + 17]);
+
+                                }
+                                else
+                                {
+                                    u--;
+                                }
+
+                            
+                            
+                        }
+                    }
+                }
+            }
+
+            document.PageSetup.TextColumns.SetCount(2);
+
+            var paragraph = document.Paragraphs.Add();
+            paragraph.Range.Font.Name = "Times New Roman";
+            paragraph.Range.Font.Size = 9;
+            paragraph.LineSpacing = 1F;
+            paragraph.Range.Text = wordDocBuilder.ToString();
+
+            string filename = textBox5.Text;
+            
+            application.ActiveDocument.SaveAs(filename, WdSaveFormat.wdFormatDocument);
+            ((Microsoft.Office.Interop.Word._Document)document).Close();
+            MessageBox.Show("File saved in Documents");
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
